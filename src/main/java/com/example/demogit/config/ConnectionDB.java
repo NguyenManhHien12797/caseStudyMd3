a0d807f4d2986ab1c1d2c9525f8614b25679dfd6
vinhng2301 <ducvinhnguyen0201@gmail.com>
2022-08-29 16:52:22 +0700

    done connection

---
+++ src/main/java/com/example/demogit/config/ConnectionDB.java
@@ -0,0 +1,34 @@
+package com.example.demogit.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+
+    private static Connection connect;
+
+    public static final String URL = "jdbc:mysql://localhost:3306/case_study";
+    public static final String USER = "root";
+    public static final String PASSWORD = "chetdicon";
+
+    protected Connection getConnect(){
+        if (connect==null){
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                connect = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("ket noi thanh cong");
+            } catch (ClassNotFoundException | SQLException e) {
+                System.out.println("ket noi khong thanh cong");
+                e.printStackTrace();
+            }
+        }
+        return connect;
+    }
+
+
+    public static void main(String[] args) {
+        ConnectionDB connectionDB = new ConnectionDB();
+        connectionDB.getConnect();
+    }
+}
