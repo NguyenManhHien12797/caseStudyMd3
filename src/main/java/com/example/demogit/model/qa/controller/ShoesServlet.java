6ff82e6d2c23e1848bf41427d4876e48c8095fc3
Quanganhhh <quanganhbui2703@gmail.com>
2022-08-30 11:46:58 +0700

    setconnecInsert

--- src/main/java/com/example/demogit/model/qa/controller/ShoesServlet.java
+++ src/main/java/com/example/demogit/model/qa/controller/ShoesServlet.java
@@ -1,6 +1,9 @@
 package com.example.demogit.model.qa.controller;
 
 import com.example.demogit.model.qa.model.Crawls;
+import com.example.demogit.model.qa.model.IShoes;
+import model.Shoes;
+import model.ShoesDAO;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,11 +12,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 
 
     @WebServlet(name = "ShoesServlet",value = "/shoes")
     public class ShoesServlet extends HttpServlet {
+        private model.ShoesDAO shoesDAO=new ShoesDAO();
         @Override
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //            listNike(request,response);
@@ -21,8 +26,6 @@ import java.util.List;
 //            listJordan(request,response);
             listYeezy(request,response);
         }
-
-
 
         @Override
         protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -55,6 +58,20 @@ import java.util.List;
             request.setAttribute("listShoesY",shoesList);
             RequestDispatcher dispatcher=request.getRequestDispatcher("qa/listYeezy.jsp");
             dispatcher.forward(request,response);
+        }
+        private void insertShoes(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException, ServletException {
+            int id = Integer.parseInt(request.getParameter("id"));
+            int categoryId = Integer.parseInt(request.getParameter("categoryId"));
+            int brandId = Integer.parseInt(request.getParameter("brandId"));
+            String name = request.getParameter("name");
+            String oldPrice = request.getParameter("oldPrice");
+            String newPrice = request.getParameter("newPrice");
+            String image = request.getParameter("image");
+
+            model.Shoes shoes = new Shoes(id,categoryId, brandId, name,oldPrice,newPrice,image);
+            shoesDAO.insertShoes(shoes);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("view/create.jsp");
+            dispatcher.forward(request, response);
         }
     }
 
