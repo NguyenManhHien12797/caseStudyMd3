f9ae22f21e28735d2a8a7f0db2ec8d93d56fa2a5
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-09-01 18:05:22 +0700

    fix admin method

--- src/main/java/com/example/caseStudyMd3/controller/AdminServlet.java
+++ src/main/java/com/example/caseStudyMd3/controller/AdminServlet.java
@@ -2,9 +2,17 @@ package com.example.caseStudyMd3.controller;
 
 import com.example.caseStudyMd3.model.Brand;
 import com.example.caseStudyMd3.model.Category;
+import com.example.caseStudyMd3.model.Product;
+import com.example.caseStudyMd3.model.Users;
+import com.example.caseStudyMd3.service.RS.Role;
+import com.example.caseStudyMd3.service.RS.Status;
 import com.example.caseStudyMd3.service.implementService.BrandService;
+import com.example.caseStudyMd3.service.implementService.CategoryService;
+import com.example.caseStudyMd3.service.implementService.ProductService;
 import com.example.caseStudyMd3.service.implementService.UserService;
 import com.example.caseStudyMd3.service.interfaceService.IBrandService;
+import com.example.caseStudyMd3.service.interfaceService.ICategoryService;
+import com.example.caseStudyMd3.service.interfaceService.IProductService;
 import com.example.caseStudyMd3.service.interfaceService.IUserService;
 
 import javax.servlet.ServletException;
@@ -13,6 +21,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -20,8 +29,8 @@ import java.util.List;
     public class AdminServlet extends HttpServlet {
         private final IUserService iUsersService = new UserService();
 
-        private final IProductService iProductService = new ProductServiceImplement();
-        private final ICategoryService categoryService = new CategoryServiceImplement();
+        private final IProductService iProductService = new ProductService();
+        private final ICategoryService categoryService = new CategoryService();
         private final IBrandService brandService = new BrandService();
         private final List<Brand> brands = brandService.getAll();
         private final List<Category> categories = categoryService.getAll();
@@ -54,15 +63,7 @@ import java.util.List;
                 case "delete":
                     deleteProduct(request,response);
                     break;
-                case "laptops":
-                    getAllLaptops(request,response);
-                    break;
-                case "smartphone":
-                    getAllSmartPhones(request,response);
-                    break;
-                case "tablet":
-                    getAllTablets(request,response);
-                    break;
+
                 case "users":
                     getAllUsers(request,response);
                     break;
@@ -81,6 +82,135 @@ import java.util.List;
                     display(request,response);
             }
 
+        }
+        private void blockedAccount(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            int id = Integer.parseInt(request.getParameter("id"));
+            Users users = iUsersService.findById(id);
+            users.setStatus(Status.BLOCKED);
+            boolean check = iUsersService.update(id,users);
+            request.setAttribute("checkBlock",check);
+            getAllUsers(request,response);
+        }
+
+        private void activeAccount(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            int id = Integer.parseInt(request.getParameter("id"));
+            Users users = iUsersService.findById(id);
+            users.setStatus(Status.ACTIVE);
+            boolean check = iUsersService.update(id,users);
+            request.setAttribute("checkActive",check);
+            getAllUsers(request,response);
+        }
+
+
+
+
+
+
+
+        private void getAllCustomer(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            List<Users> users = iUsersService.getAll();
+            List<Users> customers = new ArrayList<>();
+            for (int i = 0; i < users.size(); i++) {
+                if (users.get(i).getRole() == Role.USER){
+                    customers.add(users.get(i));
+                }
+            }
+            request.setAttribute("users", customers);
+            request.getRequestDispatcher("admin/users.jsp").forward(request,response);
+        }
+
+        private void getAllAdmins(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            List<Users> users = iUsersService.getAll();
+            List<Users> admins = new ArrayList<>();
+            for (int i = 0; i < users.size(); i++) {
+                if (users.get(i).getRole() == Role.ADMIN){
+                    admins.add(users.get(i));
+                }
+            }
+            request.setAttribute("users", admins);
+            request.getRequestDispatcher("admin/users.jsp").forward(request,response);
+        }
+
+        private void getAllUsers(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            List<Users> users = iUsersService.getAll();
+            request.setAttribute("users", users);
+            request.getRequestDispatcher("admin/users.jsp").forward(request,response);
+        }
+
+        private void display(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            List<Users> users = iUsersService.getAll();
+            List<Product> products = iProductService.getAll();
+            request.setAttribute("users", users);
+            request.setAttribute("products", products);
+            request.getRequestDispatcher("admin/system.jsp").forward(request,response);
+
+        }
+
+        private void getAllProducts(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            List<Product> products = iProductService.getAll();
+            request.setAttribute("products", products);
+            request.setAttribute("categories", categories);
+            request.setAttribute("brands", brands);
+            request.getRequestDispatcher("admin/products.jsp").forward(request,response);
+        }
+
+        private void deleteProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            int id = Integer.parseInt(request.getParameter("id"));
+            boolean checkDelete = iProductService.delete(id);
+            List<Product> products = iProductService.getAll();
+            request.setAttribute("products", products);
+            request.setAttribute("checkDel", checkDelete);
+            request.setAttribute("categories",categories);
+            request.setAttribute("brands",brands);
+            request.getRequestDispatcher("admin/products.jsp").forward(request,response);
+        }
+
+        private void editGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            int id = Integer.parseInt(request.getParameter("id"));
+            Product product = iProductService.findById(id);
+            request.setAttribute("brands",brands);
+            request.setAttribute("categories",categories);
+            request.setAttribute("product", product);
+            request.getRequestDispatcher("admin/edit.jsp").forward(request,response);
+        }
+
+        private void editProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            String serial = request.getParameter("serial");
+            String name = request.getParameter("name");
+            int categoryId = Integer.parseInt(request.getParameter("category"));
+            int brandId = Integer.parseInt(request.getParameter("brand"));
+            double price = Double.parseDouble(request.getParameter("price"));
+            int quantity = Integer.parseInt(request.getParameter("quantity"));
+            String description = request.getParameter("description");
+            String imageURL = request.getParameter("imageURL");
+            Product product = new Product(serial,name,price,quantity,description,imageURL);
+            int id = Integer.parseInt(request.getParameter("id"));
+            boolean check = iProductService.update(id , product, categoryId, brandId);
+            request.setAttribute("checkEdit", check);
+            List<Product> products = iProductService.getAll();
+            request.setAttribute("categories",categories);
+            request.setAttribute("brands",brands);
+            request.setAttribute("products", products);
+            request.getRequestDispatcher("admin/products.jsp").forward(request,response);
+        }
+
+        private void addProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            String serial = request.getParameter("serial");
+            String name = request.getParameter("name");
+            int categoryId = Integer.parseInt(request.getParameter("category"));
+            int brandId = Integer.parseInt(request.getParameter("brand"));
+            double price = Double.parseDouble(request.getParameter("price"));
+            int quantity = Integer.parseInt(request.getParameter("quantity"));
+            String description = request.getParameter("description");
+            String imageURL = request.getParameter("imageURL");
+            Product product = new Product(serial,name,price,quantity,description,imageURL);
+            boolean check = iProductService.add(product, categoryId, brandId);
+            request.setAttribute("checkAdd", check);
+            List<Product> products = iProductService.getAll();
+            request.setAttribute("products", products);
+            request.setAttribute("categories",categories);
+            request.setAttribute("brands",brands);
+            request.getRequestDispatcher("admin/products.jsp").forward(request,response);
         }
 
     }
---
+++ src/main/java/com/example/caseStudyMd3/service/implementService/CategoryService.java
@@ -0,0 +1,33 @@
+package com.example.caseStudyMd3.service.implementService;
+
+import com.example.caseStudyMd3.service.interfaceService.ICategoryService;
+
+import java.util.List;
+
+public class CategoryService implements ICategoryService {
+//    private final ICategoryDAO categoryDao = new CategoryDAOImplement();
+//    @Override
+//    public List<Category> getAll() {
+//        return categoryDao.getAll();
+//    }
+//
+//    @Override
+//    public boolean add(Category category) {
+//        return categoryDao.add(category);
+//    }
+//
+//    @Override
+//    public boolean update(int id, Category category) {
+//        return categoryDao.update(id, category);
+//    }
+//
+//    @Override
+//    public boolean delete(int id) {
+//        return categoryDao.delete(id);
+//    }
+//
+//    @Override
+//    public Category findById(int id) {
+//        return categoryDao.findById(id);
+//    }
+}
